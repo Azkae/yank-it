a4df7384cb455b44262945fcd0509d72a5f395eb
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2026-07-27 13:51:26 +0200

    run async

--- goto-source/goto-source.el
+++ goto-source/goto-source.el
@@ -17,21 +17,30 @@
            (project-root (project-current))
          default-directory))))
 
-(defun goto-source--run (selector root)
-  "Return a list of (ELEMENT FILE LINE) matches for SELECTOR under ROOT."
-  (let* ((output (shell-command-to-string
-                  (format "%s --all %s %s"
-                          goto-source-executable
-                          (shell-quote-argument selector)
-                          (shell-quote-argument root))))
-         (lines (split-string (string-trim output) "\n" t)))
-    (delq nil
-          (mapcar (lambda (line)
-                    (when (string-match "\\`\\(?:\\([^\t]+\\)\t\\)?\\(.+\\):\\([0-9]+\\)\\'" line)
-                      (list (match-string 1 line)
-                            (match-string 2 line)
-                            (string-to-number (match-string 3 line)))))
-                  lines))))
+(defun goto-source--parse (output)
+  "Parse CLI OUTPUT into a list of (ELEMENT FILE LINE) matches."
+  (delq nil
+        (mapcar (lambda (line)
+                  (when (string-match "\\`\\(?:\\([^\t]+\\)\t\\)?\\(.+\\):\\([0-9]+\\)\\'" line)
+                    (list (match-string 1 line)
+                          (match-string 2 line)
+                          (string-to-number (match-string 3 line)))))
+                (split-string (string-trim output) "\n" t))))
+
+(defun goto-source--run (selector root callback)
+  "Run goto-source on SELECTOR under ROOT, then call CALLBACK with the matches."
+  (let ((buffer (generate-new-buffer " *goto-source*")))
+    (make-process
+     :name "goto-source"
+     :buffer buffer
+     :command (list goto-source-executable "--all" selector root)
+     :noquery t
+     :sentinel
+     (lambda (proc _event)
+       (unless (process-live-p proc)
+         (let ((output (with-current-buffer buffer (buffer-string))))
+           (kill-buffer buffer)
+           (run-with-timer 0 nil callback (goto-source--parse output))))))))
 
 (defun goto-source--state ()
   "Preview state function for (ELEMENT FILE LINE) candidates."
@@ -49,7 +58,10 @@
 
 (defun goto-source--select (results root)
   "Pick one of RESULTS with consult, grouped by selector element."
-  (let ((candidates
+  ;; Bind `this-command' so vertico-repeat saves the session under a
+  ;; command it can re-execute (we may be called from a process filter).
+  (let ((this-command 'goto-source-repeat)
+        (candidates
          (mapcar (pcase-lambda ((and result `(,element ,file ,line)))
                    (cons (propertize (format "%s:%d" (file-relative-name file root) line)
                                      'goto-source-element element)
@@ -64,18 +76,33 @@
                    :preview-key '(:debounce 0.1 any)
                    :group (lambda (cand transform)
                             (if transform cand
-                              (get-text-property 0 'goto-source-element cand))))))
+                              (propertize (get-text-property 0 'goto-source-element cand)
+                                          'face 'consult-file))))))
+
+(defvar goto-source--last-selector nil
+  "Last selector visited, for `goto-source-repeat'.")
 
 (defun goto-source--visit (selector)
-  (let* ((root (goto-source--root))
-         (results (goto-source--run selector root))
-         (result (pcase (length results)
-                   (0 (user-error "goto-source: no matches"))
-                   (1 (car results))
-                   (_ (goto-source--select results root)))))
-    (find-file (nth 1 result))
-    (goto-char (point-min))
-    (forward-line (1- (nth 2 result)))))
+  (setq goto-source--last-selector selector)
+  (let ((root (goto-source--root)))
+    (goto-source--run
+     selector root
+     (lambda (results)
+       (if (null results)
+           (message "goto-source: no matches")
+         (let ((result (if (cdr results)
+                           (goto-source--select results root)
+                         (car results))))
+           (find-file (nth 1 result))
+           (goto-char (point-min))
+           (forward-line (1- (nth 2 result)))))))))
+
+;;;###autoload
+(defun goto-source-repeat ()
+  "Re-run goto-source with the most recent selector."
+  (interactive)
+  (goto-source--visit (or goto-source--last-selector
+                          (user-error "goto-source: no previous selector"))))
 
 ;;;###autoload
 (defun goto-source-from-minibuffer ()
